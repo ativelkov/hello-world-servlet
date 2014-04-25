242302f939afc60075c8ba46d0151f6c8f72f1c6
Serg Melikyan <smelikyan@mirantis.com>
2014-04-25 14:47:45 +0400

    Show connection string on demo page

--- WEB-INF/classes/HelloServlet.java
+++ WEB-INF/classes/HelloServlet.java
@@ -1,42 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.*;
-
-import javax.naming.NamingException;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
-import javax.naming.*;
-import javax.sql.*;
-import java.sql.*;
-import java.sql.SQLException;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+
 
 public class HelloServlet extends HttpServlet {
 
-    protected void service(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String hostname = getHostname();
+            String dbConnectString = getDBConnectionString();
 
-        String url = "";
-        String hostName = "";
+            render(resp.getWriter(), hostname, dbConnectString);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
 
+    private void render(PrintWriter out, String hostname, String dbConnectionString) {
+        out.println("<html><body>");
+
+        out.println("<h1>Tomcat Demo App</h1>");
+        out.println("<b>Machine:</b> " + hostname);
+        out.println("</br>");
+        out.println("<b>ConnectionString:</b> " + dbConnectionString);
+
+        out.println("</body></html>");
+        out.flush();
+        out.close();
+    }
+
+    private static String getDBConnectionString() throws Exception {
+        InitialContext cxt = new InitialContext();
+        DataSource ds = (DataSource) cxt.lookup("java:/comp/env/jdbc/postgreSqlDataSource");
+
+        return ds.getConnection().getMetaData().getURL();
+    }
+
+    private static String getHostname() throws Exception {
         try {
             Process proc = Runtime.getRuntime().exec("hostname");
             BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            hostName = reader.readLine();
-        } catch (Exception e) { out.println(e.getMessage()); }
-
-        out.println("<html>");
-        out.println("<body>");
-        out.println("<h1>Tomcat Demo App</h1>");
-        out.println("<p><b>Machine:</b> " + hostName + "</p>");
-        out.println("</html>");
-        out.println("</body>");
+            return reader.readLine();
+        } catch (Exception e) {
+            return InetAddress.getLocalHost().getHostName();
+        }
     }
-
 }
